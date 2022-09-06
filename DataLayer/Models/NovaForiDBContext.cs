e078adc9f46198fdc4bbbbc5fc22c4ec132f5fbb
nickson104 <nickhin@ntlworld.com>
2022-09-06 19:38:49 +0100

    Initial commit

---
+++ DataLayer/Models/NovaForiDBContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataLayer.Models
+{
+    public partial class NovaForiDBContext : DbContext
+    {
+        public NovaForiDBContext()
+        {
+        }
+
+        public NovaForiDBContext(DbContextOptions<NovaForiDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Task> Tasks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=NovaForiDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Task");
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DataLayer/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.Models
+{
+    public partial class Task
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int Type { get; set; }
+    }
+}
